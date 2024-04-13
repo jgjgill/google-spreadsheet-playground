d8ee2da72ad4f030482b4e6dbde83fef0964ee94
jgjgill <dbdltm22@naver.com>
2024-04-13 21:28:34 +0900

    default google spreadsheet setting

---
+++ sheet.js
@@ -0,0 +1,17 @@
+import { JWT } from "google-auth-library";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import "dotenv/config";
+
+const serviceAccountAuth = new JWT({
+  email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const spreadsheetDocId = process.env.SPREADSHEET_DOC_ID;
+
+const doc = new GoogleSpreadsheet(spreadsheetDocId, serviceAccountAuth);
+
+await doc.loadInfo();
+
+console.log(doc.title);
